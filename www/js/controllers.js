42ed2446099ce39c786ea569e18c9e0c924b47dd
holidaydes <holiday.de@gmail.com>
2016-02-26 00:07:47 +0100

    step 2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,60 @@
 angular.module('starter.controllers', [])
 
 .controller('SettingsCtrl', function($scope, $translate) {
-  $scope.changeLanguage = function(langKey){
+  $scope.changeLanguage = function(langKey) {
     $translate.use(langKey);
   };
 })
 
-.controller('ChannelsCtrl', function($scope, Channels) {
+.controller('ChannelsCtrl', function($scope, Channels, TvGuide) {
+
+  $scope.port;
+  $scope.channels;
+  $scope.port_ids = '';
+  $scope.tvGuides = '';
 
   $scope.$on('$ionicView.enter', function(e) {
-    console.error('ERR', 'there will be some funny function');
+    TvGuide.getTvGuides($scope.port, $scope.port_ids, $scope.getDate());
+    $scope.tvGuides = TvGuide.channelTvGuides;
+    console.log('Query tv guides');
   });
 
   $scope.$watch(angular.bind(Channels, function() {
-   return Channels.all();
- }), function(value) {
-   if (value) {
-     $scope.channels = Channels.all();
-   }
- });
+    return Channels.channels();
+  }), function(value) {
+    if (value) {
+      $scope.port = Channels.port();
+      $scope.channels = Channels.channels();
+      for(var i = 0; i < $scope.channels.length; i++)
+      {
+        $scope.port_ids += $scope.channels[i].port_id + ((!($scope.channels.length - 1)) ? '' : ',');
+      }
+    }
+  });
+
+  $scope.getDate = function(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ((date.getMonth() < 9) ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1));
+    var day = ((date.getDate() < 10) ? '0' + date.getDate() : date.getDate());
+    return year + '-' + month + '-' + day;
+  };
+
+  $scope.getHours = function(){
+    var date = new Date();
+    var hours = ((date.getHours() < 10) ? '0' + date.getHours() : date.getHours());
+    return hours;
+  };
+
+  $scope.getMinutes = function(){
+    var date = new Date();
+    var minutes = ((date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes());
+    return minutes;
+  };
 
 })
 
 .controller('ChannelsDetailCtrl', function($scope, $stateParams, Channels) {
-  $scope.channel = Channels.get($stateParams.channelId);
-})
+  $scope.port = Channels.port();
+  $scope.channel = Channels.getChannel($stateParams.channelId);
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,27 +1,56 @@
 angular.module('starter.services', [])
 
 .factory('Channels', function($http) {
-
   var vm = this;
-  vm.channels = null;
+
+  vm.data = {};
 
   $http.get('/resource/channels.json').then(function(resp) {
-    vm.channels = resp.data;
+    vm.data = resp.data;
   }, function(err) {
     console.error('ERR', err);
   });
 
   return {
-    all: function() {
-      return vm.channels;
+    channels: function() {
+      return vm.data.channels;
     },
-    get: function(channelId) {
-      for (var i = 0; i < vm.channels.length; i++) {
-        if (vm.channels[i].id === parseInt(channelId)) {
-          return vm.channels[i];
+    port: function() {
+      return vm.data.port;
+    },
+    getChannel: function(channelId) {
+      for (var i = 0; i < vm.data.channels.length; i++) {
+        if (vm.data.channels[i].id === parseInt(channelId)) {
+          return vm.data.channels[i];
         }
       }
       return null;
     }
   };
-});
+})
+
+.service('TvGuide', ['$http', function($http) {
+
+  var vm = this;
+  vm.channelTvGuide;
+  vm.channelsTvGuide;
+
+  vm.getTvGuide = function(port, port_id, date) {
+    var url = port + port_id + '&i_portdate=' + date;
+    $http.get(url).then(function(resp) {
+      vm.channelTvGuide = resp.data;
+    }, function(err) {
+      console.error('ERR', err);
+    });
+  };
+
+  vm.getTvGuides = function(port, port_ids, date) {
+    var url = port + port_ids + '&i_portdate=' + date;
+    $http.get(url).then(function(resp) {
+      vm.channelTvGuides = resp.data;
+    }, function(err) {
+      console.error('ERR', err);
+    });
+  };
+
+}]);
